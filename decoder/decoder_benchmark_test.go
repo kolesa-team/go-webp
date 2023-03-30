1d3590de04970a7e000552d804894a45be55a567
Travis Grigsby <tgrigsby@snapchat.com>
2023-03-30 13:32:38 -0700

    simplifying after some more testing

--- decoder/decoder_benchmark_test.go
+++ decoder/decoder_benchmark_test.go
@@ -9,32 +9,8 @@ import (
 	"github.com/kolesa-team/go-webp/pool"
 )
 
-func BenchmarkDecodeOneSizeOnly(b *testing.B) {
-	b.ResetTimer()
-	b.ReportAllocs()
-
-	data := loadImage(b)
-
-	nrgbaPool := pool.NewNRGBAOneSizePool(512, 512)
-	bufferPool := pool.NewBufferPool(512 * 512)
-
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			buf := bufferPool.Get()
-			decoder, err := NewDecoder(bytes.NewReader(data), &Options{imageFactory: nrgbaPool, buffer: buf})
-			img, err := decoder.Decode()
-			if err != nil {
-				b.Fatal(err)
-			}
-			nrgbaPool.Put(img.(*image.NRGBA))
-			bufferPool.Put(buf)
-		}
-	})
-}
-
 func loadImage(b *testing.B) []byte {
 	filename := "../test_data/images/100x150_lossless.webp"
-	// filename := "../test_data/images/composite.webp"
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		b.Fatal(err)
@@ -42,30 +18,32 @@ func loadImage(b *testing.B) []byte {
 	return data
 }
 
-func BenchmarkDecodeAnySize(b *testing.B) {
+func BenchmarkDecodePooled(b *testing.B) {
 	b.ResetTimer()
 	b.ReportAllocs()
 
 	data := loadImage(b)
 
-	nrgbaPool := pool.NewNRGBAMultiPool()
-	bufferPool := pool.NewBufferPool(512 * 512)
+	imagePool := pool.NewSimplePool()
+	bufferPool := pool.NewBufferPool()
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			buf := bufferPool.Get()
-			decoder, err := NewDecoder(bytes.NewReader(data), &Options{imageFactory: nrgbaPool, buffer: buf})
+			decoder, err := NewDecoder(bytes.NewReader(data), &Options{imageFactory: imagePool, buffer: buf})
 			img, err := decoder.Decode()
 			if err != nil {
 				b.Fatal(err)
 			}
-			nrgbaPool.Put(img.(*image.NRGBA))
+
+			// put everything back
+			imagePool.Put(img.(*image.NRGBA))
 			bufferPool.Put(buf)
 		}
 	})
 }
 
-func BenchmarkDecodeOriginal(b *testing.B) {
+func BenchmarkDecodeUnPooled(b *testing.B) {
 	b.ResetTimer()
 	b.ReportAllocs()
 
